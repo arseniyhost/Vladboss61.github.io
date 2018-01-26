7a122dcff497b12ffcfe0b8162656bf66f010948
Vlad Varvahsenko <vlad19972017@gmail.com>
2018-01-26 13:11:41 +0200

    Starting do first blog, day 1. Did some css for view and pointed where will have to generate html on click + some menu

--- Js-code/jscode.js
+++ Js-code/jscode.js
@@ -1,3 +1,42 @@
-f = function () {
+$(document).ready(function () {
+    var trigger = $('.hamburger'),
+        overlay = $('.overlay'),
+        isClosed = false;
 
-};
+    trigger.click(function () {
+        hamburger_cross();
+    });
+
+    function hamburger_cross() {
+
+        if (isClosed == true) {
+            overlay.hide();
+            trigger.removeClass('is-open');
+            trigger.addClass('is-closed');
+            isClosed = false;
+        } else {
+            overlay.show();
+            trigger.removeClass('is-closed');
+            trigger.addClass('is-open');
+            isClosed = true;
+        }
+    }
+
+    $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+    });
+
+    $('li.sidebar-brand a').click(function () {
+        $('.me-info').html()// сгенерить разметку при выборе меня
+    })
+
+    $('#stories').click( function () {
+        $('.me-info').html()// сгенерить разметку при выборе историй
+    });
+    $('#about-me').click( function () {
+        $('.me-info').html()// сгенерить разметку при выборе инфы о мне
+    });
+    $('#social-network').click( function () {
+        $('.me-info').html()// сгенерить разметку при  социальных ссылок
+    });
+});
