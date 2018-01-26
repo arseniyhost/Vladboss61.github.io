535ea450e21b6bfccb245e4235fe370622d24c58
Vlad Varvahsenko <vlad19972017@gmail.com>
2018-01-26 14:07:04 +0200

    Starting do first blog, day 1 . Did some css for view and pointed where will have to generate html on click + some + menu  + 'Smth soon...'

--- Js-code/jscode.js
+++ Js-code/jscode.js
@@ -1,26 +1,6 @@
 $(document).ready(function () {
-    var trigger = $('.hamburger'),
-        overlay = $('.overlay'),
-        isClosed = false;
-
-    trigger.click(function () {
-        hamburger_cross();
-    });
-
-    function hamburger_cross() {
-
-        if (isClosed == true) {
-            overlay.hide();
-            trigger.removeClass('is-open');
-            trigger.addClass('is-closed');
-            isClosed = false;
-        } else {
-            overlay.show();
-            trigger.removeClass('is-closed');
-            trigger.addClass('is-open');
-            isClosed = true;
-        }
-    }
+    MenuSlider();
+    AnimationSmthSoon();
 
     $('[data-toggle="offcanvas"]').click(function () {
         $('#wrapper').toggleClass('toggled');
@@ -39,4 +19,49 @@ $(document).ready(function () {
     $('#social-network').click( function () {
         $('.me-info').html();// сгенерить разметку при  социальных ссылок
     });
+
+    function MenuSlider() {
+        var trigger = $('.hamburger'),
+            overlay = $('.overlay'),
+            isClosed = false;
+        trigger.click(function () {
+            hamburger_cross();
+        });
+        function hamburger_cross() {
+            if (isClosed == true) {
+                overlay.hide();
+                trigger.removeClass('is-open');
+                trigger.addClass('is-closed');
+                isClosed = false;
+            } else {
+                overlay.show();
+                trigger.removeClass('is-closed');
+                trigger.addClass('is-open');
+                isClosed = true;
+            }
+        }
+    }
+    // делаем типа анимацию smth Soon. .. ...
+    function AnimationSmthSoon() {
+        var tick = 0;
+        setInterval(function () {
+            var soon = $('div.col-lg-8');
+            switch (tick){
+                case 0:
+                    tick++;
+                    soon.html('<h1>Vlad Varvashenko | Blog</h1><h2>Smth soon.</h2>');
+                    break;
+                case 1:
+                    tick++;
+                    soon.html('<h1>Vlad Varvashenko | Blog</h1><h2>Smth soon..</h2>');
+                    break;
+                case 2:
+                    tick++;
+                    soon.html('<h1>Vlad Varvashenko | Blog</h1><h2>Smth soon...</h2>');
+                    break;
+                default:
+                    tick = 0;
+            }
+        },800);
+    }
 });
