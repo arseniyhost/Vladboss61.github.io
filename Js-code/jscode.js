0924cfac4c3ae4c91d45ffdb0eb5a689c657b827
Vlad Varvahsenko <vlad19972017@gmail.com>
2018-01-27 15:06:45 +0200

    добавил ссылки на соц сети надо исправить один баг, с нажатием на фото Меня что бы клацалось и появлялось и пропадала ВСПОМНИ ПОТОМ!'

--- Js-code/jscode.js
+++ Js-code/jscode.js
@@ -1,25 +1,55 @@
 $(document).ready(function () {
-    AnimationSmthSoon();
+//main
+    //AnimationSmthSoon();
+    clickedOnMainPhoto();
     MenuSlider();
+
+
 
     $('[data-toggle="offcanvas"]').click(function () {
         $('#wrapper').toggleClass('toggled');
     });
 
     $('li.sidebar-brand a').click(function () {
-        $('.me-info').html();// сгенерить разметку при выборе меня
+        $('.me-info').html(' ');// сгенерить разметку при выборе меня
     })
 
     $('#stories').click( function () {
-        $('.me-info').html();// сгенерить разметку при выборе историй
+        $('.me-info').html(' ');// сгенерить разметку при выборе историй
     });
     $('#about-me').click( function () {
-        $('.me-info').html();// сгенерить разметку при выборе инфы о мне
+        $('.me-info').html(' ');// сгенерить разметку при выборе инфы о мне
     });
     $('#social-network').click( function () {
-        $('.me-info').html();// сгенерить разметку при  социальных ссылок
+        var html = `<div class="social-net">                   
+                    <ul>
+                        <li>
+                            <a href="http://t.me/VladikDRow" target="_blank"> 
+                                <img class="img-circle" src="images\\icons-social-networks\\telegram_icon-icons.com_72055.png" alt="">
+                            </a>
+                        </li>
+                        <li id="git-logo">
+                            <a href="https://github.com/vladboss61" target="_blank">
+                                <img  class="img-circle" src="images\\icons-social-networks\\Github.png" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.instagram.com/evilvladik/" target="_blank">
+                            <img class="img-rounded" src="images\\icons-social-networks\\instagram_socialnetwork_20033.png" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/FlexLikeVlad" target="_blank">
+                            <img class="img-circle" src="images\\icons-social-networks\\twitter_socialnetwork_20007.png" alt="">
+                            </a>
+                        </li>
+                    </ul>
+                </div>`;
+        $('.me-info').html(html);// сгенерить разметку при  социальных ссылок
     });
 
+
+//~main
     function MenuSlider() {
         var trigger = $('.hamburger'),
             overlay = $('.overlay'),
@@ -41,23 +71,39 @@ $(document).ready(function () {
             }
         }
     }
+    function clickedOnMainPhoto() {
+        var flag = true;
+        $(document).on('click','.col-lg-8 img.img-circle', function () {
+            if(flag){
+                $('.col-lg-8 img.img-circle + div').addClass('smth-soon');
+                AnimationSmthSoon();
+                flag = false;
+            }else {
+                $('div.col-lg-8 .smth-soon').html(' ');
+                $('.col-lg-8 img.img-circle + div').removeClass()('smth-soon');
+                flag = true;
+            }
+        });
+    }
+
+
     // делаем типа анимацию smth Soon. .. ...
     function AnimationSmthSoon() {
         var tick = 0;
         setInterval(function () {
-            var soon = $('div.col-lg-8');
+            var soon = $('div.col-lg-8 div.smth-soon');
             switch (tick){
                 case 0:
                     tick++;
-                    soon.html('<h1>Vlad Varvashenko | Blog</h1><h2>Smth soon.</h2>');
+                    soon.html('<h2>Smth soon.</h2>');
                     break;
                 case 1:
                     tick++;
-                    soon.html('<h1>Vlad Varvashenko | Blog</h1><h2>Smth soon..</h2>');
+                    soon.html('<h2>Smth soon..</h2>');
                     break;
                 case 2:
                     tick++;
-                    soon.html('<h1>Vlad Varvashenko | Blog</h1><h2>Smth soon...</h2>');
+                    soon.html('<h2>Smth soon...</h2>');
                     break;
                 default:
                     tick = 0;
