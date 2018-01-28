5881339563de4b4d6174668779febfeff1feee95
Vlad Varvahsenko <vlad19972017@gmail.com>
2018-01-28 19:04:58 +0200

    Добавил анимацию подпрыгивания буквы V

--- Js-code/js-animate.js
+++ Js-code/js-animate.js
@@ -1,3 +1,15 @@
-    $(document).ready(function () {
-
-    });
+$(document).ready(function () {
+    Jump(true, $('span.v-jump'));
+});
+function Jump(flag, obj) {
+    var jump = obj;
+    setInterval(function () {
+        if(flag){
+            jump.addClass('paused');
+            flag = false;
+        }else {
+            jump.removeClass('paused');
+            flag = true;
+        }
+    },3000);
+}
